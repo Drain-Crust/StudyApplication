c18e37666d1bdd65676784cb88c9363a2dbb423a
17162 <171>
2021-05-06 10:28:47 +1200

    added email and password system that isnt linked yet to a data base or other.

--- app/src/main/java/com/example/studyapplication/MainActivity.java
+++ app/src/main/java/com/example/studyapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +14,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button login_button = (Button) findViewById(R.id.login_button);
+        Button login_button = findViewById(R.id.login_button);
         login_button.setTransformationMethod(null);
     }
+
+
+
     public void toHomeScreen (View aView){
         Intent intent = new Intent(this,HomeScreen.class);
-        startActivity(intent);
+        EditText email = findViewById(R.id.emailAddressText);
+        EditText password = findViewById(R.id.passwordText);
+        String email_string = email.getText().toString();
+        String password_string = password.getText().toString();
+        switch (email_string){
+            case "correct":
+                System.out.println("correct email");
+                switch (password_string){
+                    case "correct":
+                        System.out.print("correct password");
+                        startActivity(intent);
+                        break;
+                    case "incorrect":
+                        System.out.println("incorrect password");
+                        break;
+                }
+                break;
+            case "incorrect":
+                System.out.println("incorrect email");
+                break;
+            default:
+                System.out.println("didn't work");
+        }
     }
 }
